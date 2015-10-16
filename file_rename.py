c740ff4a8b53269de8cb9f7ca7a06413e77bbb5f
han <Han Qi>
2015-10-16 15:52:25 -0500

    more pages

--- file_rename.py
+++ file_rename.py
@@ -4,13 +4,20 @@ import shutil
 
 
 def main():
-    i = 2 
+    i = 1
     for x in os.listdir(sys.argv[1]):
-        if x.startswith('IMG'):
-            filename = '{}.jpg'.format(i)
-            filename = os.path.join(sys.argv[1], filename)
-            src = os.path.join(sys.argv[1], x)
-            shutil.move(src, filename)
-            i += 1
+        filename = 'a{}.jpg'.format(i)
+        filename = os.path.join(sys.argv[1], filename)
+        src = os.path.join(sys.argv[1], x)
+        shutil.move(src, filename)
+        i += 1
+
+    i = 1
+    for x in os.listdir(sys.argv[1]):
+        filename = '{}.jpg'.format(i)
+        filename = os.path.join(sys.argv[1], filename)
+        src = os.path.join(sys.argv[1], x)
+        shutil.move(src, filename)
+        i += 1
 
 main()
